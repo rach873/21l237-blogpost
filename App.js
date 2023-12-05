5cddc606fd98276faa2fc7ed70d796f7595fcffc
rach873 <122551813+rach873@users.noreply.github.com>
2023-12-05 23:37:31 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Header from './components/Header';
+import PostList from './components/PostList';
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <PostList />
+    </div>
+  );
+}
+export default App;
